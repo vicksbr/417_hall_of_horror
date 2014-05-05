6c573696f3d07b0fe0ccb6e2a554de97025c1e44
vicksbr <pedropuzzi@gmail.com>
2014-05-05 01:53:09 -0300

    build x0417

--- 417game.h
+++ 417game.h
@@ -13,6 +13,17 @@ typedef struct Monster Monster;
 int Monster_attack(void *self, int damage);
 int Monster_init(void *self);
 
+
+struct Item {
+
+    Object proto;
+
+};
+
+typedef struct Item Item;
+int Item_init(void *self);
+
+
 struct Room {
     Object proto;
     Monster *bad_guy;
@@ -20,7 +31,7 @@ struct Room {
     struct Room *south;
     struct Room *east;
     struct Room *west;
-    struct Item *item;
+    Item *item;
 };
 
 typedef struct Room Room;
@@ -42,15 +53,6 @@ void *Map_move(void *self, Direction direction);
 int Map_attack(void *self, int damage);
 int Map_init(void *self);
 
-
-struct Item {
-
-    Object proto;
-
-};
-
-typedef struct Item Item;
-int Item_init(void *self);
 
 
 
