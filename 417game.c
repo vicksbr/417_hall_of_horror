a8a43aa15c245deab53d51822972ca7469078aa3
vicksbr <pedropuzzi@gmail.com>
2016-05-06 15:07:30 -0300

    Condicao de fim de jogo criada

--- 417game.c
+++ 417game.c
@@ -1,16 +1,15 @@
-	#include <stdio.h>
-	#include <errno.h>
-	#include <stdlib.h>
-	#include <string.h>
-	#include <time.h>
-	#include "417game.h"
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "417game.h"
+#define cor_verde "\033[32m"
 
-	#define cor_verde "\033[32m"
 
+void primeira_visita(char *sala) {
 
-	void primeira_visita(char *sala) {
-
-	    if (strcmp(sala,"Quintal") == 0)
+	if (strcmp(sala,"Quintal") == 0)
 		printf("Nossa, veaks, ta sol aqui fora");
 
 	    if (strcmp(sala,"Calabouço") == 0)
@@ -22,461 +21,481 @@
 	    if (strcmp(sala,"Modulo 2") == 0)
 		printf("Sala de muitas loucuras!");
 	    
-	}
+}
 
 
-	int Player_init(void *self)
-	{
+int Player_init(void *self)
+{
 
-	    Player *player = self;
-	    player->hit_points = 10;
-	    player->nitens = 0;
-	    player->itemAtual = NULL;
+    Player *player = self;
+    player->hit_points = 10;
+    player->nitens = 0;
+    player->itemAtual = NULL;
+    return 1;
+
+}
+
+void Player_describe(void *self) {
+    Player *player = self;
+    printf("\tNome: %s\n",player->_(description));
+    printf("\tHitpoints: %d\n",player->hit_points);
+}
+
+
+int Player_equip_item(void *self,void *selfItem) {
+
+    Player *player = self;
+    player->itemAtual = selfItem;
+    return 1;
+}
+
+void Player_remove_item(void *self,char *descricao) {
+
+    Player *player = self;
+
+    int i;
+
+    for(i = 0; i < player->nitens; i++) {
+
+	if (strcmp(player->itens[i].proto.description,descricao) == 0)
+
+	    free(&player->itens[i]);
+      
+    }
+}
+
+
+void Player_add_item(void *self,void *item_self) {
+
+    Player *player = self;
+    Item *item = item_self;
+
+    int i = player->nitens;
+
+    if (item) {
+
+	player->itens[i] = *item;
+	printf("peguei %s\n",player->itens[i].proto.description);
+	player->nitens++;
+
+    }
+}
+
+void Player_get_item(void *mapa) {
+
+
+    Map *map = mapa;
+    Item *item = map->location->item;
+
+     Player_add_item(map->player,item);
+
+     free(map->location->item);
+    map->location->item = NULL;
+}
+
+
+int Player_itemValidate(void *self,char *item) {
+
+    Player *player = self;
+
+    int i;
+
+    for (i = 0; i < player->nitens; i++) {
+
+	if (strcmp(player->itens[i].proto.description,item) == 0) {
 	    return 1;
-
-	}
-
-	void Player_describe(void *self) {
-	    Player *player = self;
-	    printf("\tNome: %s\n",player->_(description));
-	    printf("\tHitpoints: %d\n",player->hit_points);
 	}
 
 
-	int Player_equip_item(void *self,void *selfItem) {
+    }
+    return 0;
+}
 
-	    Player *player = self;
-	    player->itemAtual = selfItem;
+
+void Player_joint_roll(void *self) {
+
+    printf("\tBolando um baseado\n");
+    Player *player = self;
+    Item *item = player->itens;
+    strcpy(item[0].proto.description,"baseado");
+    strcpy(item[1].proto.description,"");
+    strcpy(item[2].proto.description,"");
+}
+
+void Player_describe_list(void *self) {
+
+    Player *player = self;
+
+    int i;
+    for (i=0; i < player->nitens; i++) {
+	printf("\titem[%d]: %s\n",i,player->itens[i].proto.description);
+    }
+
+}
+
+
+Object PlayerProto = {
+
+    .init = Player_init,
+    .describe = Player_describe
+};
+
+
+int Item_init(void *self)
+{
+    return 1;
+
+}
+
+Object ItemProto = {
+
+    .init = Item_init
+
+};
+
+
+int Monster_attack(void *self, int damage)
+{
+    Map *map = self;
+    Room *room = map->location;
+    Player *player = map->player;
+    Monster *monster = room->bad_guy;
+    Item *item = monster->item;
+
+    if (strcmp(monster->_(description),"O insaciavel kchaça") == 0) {
+
+
+	printf("Você atacou %s!\n", monster->_(description));
+	monster->hit_points -= damage;
+
+	if(monster->hit_points > 0) {
+	    printf("Não esta chapado ainda.\n");
+	    return 0;
+	}
+	else if (monster->hit_points <= 0 && monster->status == 0) {
+	    printf("Não acredito veeaaaks, chapei!!!\n");
+	    monster->status = 1;
+	    Player_add_item(player,item);
 	    return 1;
 	}
+	else {
 
-	void Player_remove_item(void *self,char *descricao) {
-
-	    Player *player = self;
-
-	    int i;
-
-	    for(i = 0; i < player->nitens; i++) {
-
-		if (strcmp(player->itens[i].proto.description,descricao) == 0)
-
-		    free(&player->itens[i]);
-	      
-	    }
-	}
-
-
-	void Player_add_item(void *self,void *item_self) {
-
-	    Player *player = self;
-	    Item *item = item_self;
-
-	    int i = player->nitens;
-
-	    if (item) {
-
-		player->itens[i] = *item;
-		printf("peguei %s\n",player->itens[i].proto.description);
-		player->nitens++;
-
-	    }
-	}
-
-	void Player_get_item(void *mapa) {
-
-
-	    Map *map = mapa;
-	    Item *item = map->location->item;
-
-	     Player_add_item(map->player,item);
-
-	     free(map->location->item);
-	    map->location->item = NULL;
-	}
-
-
-	int Player_itemValidate(void *self,char *item) {
-
-	    Player *player = self;
-
-	    int i;
-
-	    for (i = 0; i < player->nitens; i++) {
-
-		if (strcmp(player->itens[i].proto.description,item) == 0) {
-		    return 1;
-		}
-
-
-	    }
+	    printf("to chapado!!\n");
 	    return 0;
 	}
 
+    }
+    else {
+	printf("nao tem como chapar sem baseado!!\n");
+	return 0;
+    }
+}
 
-	void Player_joint_roll(void *self) {
+int Monster_init(void *self) 
+{
+    Monster *monster = self;
+    monster->hit_points = 10;
+    monster->status = 0;
+    return 1;
+}
 
-	    printf("\tBolando um baseado\n");
-	    Player *player = self;
-	    Item *item = player->itens;
-	    strcpy(item[0].proto.description,"baseado");
-	    strcpy(item[1].proto.description,"");
-	    strcpy(item[2].proto.description,"");
-	}
+Object MonsterProto = 
+{
+    .init = Monster_init,
+    .attack = Monster_attack
+};
 
-	void Player_describe_list(void *self) {
+int Room_item(void *self) 
+{
 
-	    Player *player = self;
+    Room *room = self;
+    Item *item = room->item;
+    if (item) {
+	printf("Item na sala: ");
+	item->_(describe)(item);
 
-	    int i;
-	    for (i=0; i < player->nitens; i++) {
-		printf("\titem[%d]: %s\n",i,player->itens[i].proto.description);
-	    }
-
-	}
-
-
-	Object PlayerProto = {
-
-	    .init = Player_init,
-	    .describe = Player_describe
-	};
-
-
-	int Item_init(void *self)
-	{
-	    return 1;
-
-	}
-
-	Object ItemProto = {
-
-	    .init = Item_init
-
-	};
+	return 1;
+    }
+    else
+	return 0;
+}
 
 
-	int Monster_attack(void *self, int damage)
-	{
-	    Map *map = self;
-	    Room *room = map->location;
-	    Player *player = map->player;
-	    Monster *monster = room->bad_guy;
-	    Item *item = monster->item;
-
-	    if (strcmp(monster->_(description),"O insaciavel kchaça") == 0) {
+void *Room_move(void *self, Direction direction)
+{
+    Map *map = self;
+    Room *room = map->location;
+    Room *next = NULL;
 
 
-		printf("Você atacou %s!\n", monster->_(description));
-		monster->hit_points -= damage;
-
-		if(monster->hit_points > 0) {
-		    printf("Não esta chapado ainda.\n");
-		    return 0;
-		}
-		else if (monster->hit_points <= 0 && monster->status == 0) {
-		    printf("Não acredito veeaaaks, chapei!!!\n");
-		    monster->status = 1;
-		    Player_add_item(player,item);
-		    return 1;
-		}
-		else {
-
-		    printf("to chapado!!\n");
-		    return 0;
-		}
-
-	    }
-	    else {
-		printf("nao tem como chapar sem baseado!!\n");
-		return 0;
-	    }
-	}
-
-	int Monster_init(void *self)
-	{
-	    Monster *monster = self;
-	    monster->hit_points = 10;
-	    monster->status = 0;
-	    return 1;
-	}
-
-	Object MonsterProto = {
-	    .init = Monster_init,
-	    .attack = Monster_attack
-	};
-
-	int Room_item(void *self) {
-
-	    Room *room = self;
-	    Item *item = room->item;
-	    if (item) {
-		printf("Item na sala: ");
-		item->_(describe)(item);
-
-		return 1;
-	    }
-	    else
-		return 0;
-	}
-
-
-	void *Room_move(void *self, Direction direction)
-	{
-	    Map *map = self;
-	    Room *room = map->location;
-	    Room *next = NULL;
-
-
-	    if(direction == NORTH && room->north) {
+    if(direction == NORTH && room->north) {
 		next = room->north;
-	    } else if(direction == SOUTH && room->south) {
+    } 
+    else if(direction == SOUTH && room->south) {
 		next = room->south;
-	    } else if(direction == EAST && room->east) {
+    } 
+    else if(direction == EAST && room->east) {
 		next = room->east;
-	    } else if(direction == WEST && room->west) {
+    } 
+    else if(direction == WEST && room->west) {
 		next = room->west;
-	    } else  {
+    } 
+    else  {
 		printf("Não pode ir nessa direção.");
 		next = NULL;
 		return next;
-	    }    
+    }    
 
-	    if (next && next->lock == 1) {
-		if (Room_open(map,next) == 1) {
-		    printf("Abrindo porta\n");
-		    printf("você foi em direção a(o):");
-		}
-		else {
-		    printf("Você precisa de uma chave!!!\n");
-		    next=NULL;
-		    return next;	
-		}
-	    }
-	    else if (next && next->lock == 0) {
-		printf("você foi em direção a(o):");
-	    }
-	    
-	    if(next) {
-		next->_(describe)(next);
-		Room_item(next);
-
-	    }
-	    if (next->visitada == 0) {
-
-		//printf("sua primeira vez aqui? xD");
-		primeira_visita(next->_(description));
-
-		next->visitada = 1;
-	    }
-	    return next;
+    if (next && next->lock == 1) {
+	if (Room_open(map,next) == 1) {
+	    printf("Abrindo porta\n");
+	    printf("você foi em direção a(o):");
 	}
-
-
-	int Room_attack(void *self, int damage)
-	{
-	    Map *map = self;
-
-	    Room *room = map->location;
-	    Monster *monster = room->bad_guy;
-
-	    if(monster) {
-		monster->_(attack)(map, damage);
-		return 1;
-	    }
-	    else {
-		printf("Você não tem quem atacar, amigão.\n");
-		return 0;
-	    }
+	else {
+	    printf("Você precisa de uma chave!!!\n");
+	    next=NULL;
+	    return next;	
 	}
+    }
+    else if (next && next->lock == 0) {
+	printf("você foi em direção a(o):");
+    }
+    
+    if(next) {
+	next->_(describe)(next);
+	Room_item(next);
+
+    }
+    if (next->visitada == 0) {
+
+	//printf("sua primeira vez aqui? xD");
+	primeira_visita(next->_(description));
+
+	next->visitada = 1;
+    }
+    return next;
+}
 
 
-	int Room_init(void *self) {
+int Room_attack(void *self, int damage)
+{
+    Map *map = self;
 
-	    Room *room = self;
-	    room->lock = 0; //nao inicia trancada
-	    room->visitada = 0; //nao inicia visitada
-	    return 1;
-	}
+    Room *room = map->location;
+    Monster *monster = room->bad_guy;
 
-
-	int Room_open(void *self,void *self2) {
-
-	    Map *map = self;
-	    Room *room = self2;
-	    Player *player = map->player;
-	    if (strcmp(room->_(description),"Quintal") == 0 && Player_itemValidate(player,"chave")) {
-		room->lock = 0;
-		return 1;
-	    }
-
-	    return 0;
-	}
+    if(monster) {
+	monster->_(attack)(map, damage);
+	return 1;
+    }
+    else {
+	printf("Você não tem quem atacar, amigão.\n");
+	return 0;
+    }
+}
 
 
-	Object RoomProto = {
-	    .move = Room_move,
-	    .attack = Room_attack
-	};
+int Room_init(void *self) {
+
+    Room *room = self;
+    room->lock = 0; //nao inicia trancada
+    room->visitada = 0; //nao inicia visitada
+    return 1;
+}
 
 
-	void *Map_move(void *self, Direction direction)
-	{
-	    Map *map = self;
-	    Room *location = map->location;
-	    Room *next = NULL;
+int Room_open(void *self,void *self2) {
 
-	    next = location->_(move)(map, direction);
+    Map *map = self;
+    Room *room = self2;
+    Player *player = map->player;
+    if (strcmp(room->_(description),"Quintal") == 0 && Player_itemValidate(player,"chave")) {
+	room->lock = 0;
+	return 1;
+    }
 
-	    if(next) {
-		map->location = next;
-	    }
-
-	    return next;
-	}
-
-	int Map_attack(void *self, int damage)
-	{
-	    Map* map = self;
-	    Room *location = map->location;
-
-	    return location->_(attack)(map, damage);
-
-	}
-
-	int Map_init(void *self)
-	{
-	    Map *map = self;
-
-	    // instancia as salas
-	    Room *hall = NEW(Room, "The 417 Hall");
-	    Room *arena = NEW(Room, "Calabouço");
-	    Room *modulo1 = NEW(Room, "Modulo 1");
-	    Room *modulo2 = NEW(Room, "Modulo2");
-	    Room *modulo3 = NEW(Room, "Modulo3");
-	    Room *quintal = NEW(Room, "Quintal");
-
-	    //instancia o player
-
-	    Player *player = NEW(Player,"vicks, the Hit-Maker");
-	    map->player = player;
-	    map->player->itens = (Item*)malloc(5*sizeof(Item));
-
-	    //instancia os itens
-	    Item *dixava = NEW(Item,"dixavador");
-	    modulo1->item = dixava;
-
-	    Item *seda = NEW(Item,"seda");
-	    modulo2->item = seda;
-
-	    Item *maconha = NEW(Item,"maconha");
-	    modulo3->item = maconha;
-
-	    // coloca nosso grande amigo cachaça na cena!
-
-	    arena->bad_guy = NEW(Monster, "O insaciavel kchaça");
-	    Item *chave = NEW(Item,"chave");
-	    arena->bad_guy->item = chave;
-
-	    // aponta os endereços das salas
-	    hall->west = modulo3;
-	    hall->north = modulo1;
-	    
-	    modulo3->east = hall;
-
-	    modulo1->west = arena;
-	    modulo1->east = modulo2;
-	    modulo1->south = hall;
+    return 0;
+}
 
 
-	    arena->east = modulo1;
-	    arena->south = quintal;
+Object RoomProto = {
+    .move = Room_move,
+    .attack = Room_attack
+};
 
-	    modulo2->west = modulo1;
 
-	    quintal->north = arena;
-	    quintal->lock = 1;
+void *Map_move(void *self, Direction direction)
+{
+    Map *map = self;
+    Room *location = map->location;
+    Room *next = NULL;
 
-	    // começa o jogo colocando o personagem no hall
-	    map->start = hall;
-	    map->location = hall;
+    next = location->_(move)(map, direction);
 
-	    //Testes
-	    //Item *banza = NEW(Item,"baseado");
-	 
-	    //Player_add_item(map->player,chave);
-	    //Player_add_item(map->player,banza );
+    if(next) {
+	map->location = next;
+    }
 
-	    return 1;
-	}
+    return next;
+}
 
-	void Map_graph(void *self) { 
+int Map_attack(void *self, int damage)
+{
+    Map* map = self;
+    Room *location = map->location;
 
-	    Map *map = self;	
-	    printf("\n");
-	    printf("\n");    
-	    printf("\n");    
-	    if (map->location->north) {
-		printf("               ");
-		printf("%s\n",map->location->north->proto.description);
-		printf("                  |                  \n");
-		printf("                  |                  \n");
-		printf("                  |                  \n");
-	    }
-	    else {    	
-		printf("               não tem\n");
-		printf("                  |                  \n");
-		printf("                  |                  \n");
-		printf("                  |                  \n");
-	    }
-	    if (map->location->west) {	     
-		 printf("%s",map->location->west->proto.description);    	 
-		 printf("———————");     
-	    }
-	    else {
-		printf("não tem——————");
-	    }
-	    printf("%s",map->location->proto.description);            
-	    
-	    if (map->location->east) { 	
-		printf("——————");
-		map->location->_(describe)(map->location->east);    
-	    }
-	    else {
-		printf("——————não tem\n");
-	    }
-	    printf("                  |                  \n");
-	    printf("                  |                  \n");
-	    printf("                  |                  \n");    
-	    if (map->location->south) {	    
-		    printf("               ");
-		    printf("%s",map->location->south->proto.description);    	 
-		printf("               \n");
-	    }
-	    else {
-		printf("               não tem               \n");
-	    }
-	}
+    return location->_(attack)(map, damage);
+
+}
+
+int Map_init(void *self)
+{
+    Map *map = self;
+
+    // instancia as salas
+    Room *hall = NEW(Room, "The 417 Hall");
+    Room *arena = NEW(Room, "Calabouço");
+    Room *modulo1 = NEW(Room, "Modulo 1");
+    Room *modulo2 = NEW(Room, "Modulo2");
+    Room *modulo3 = NEW(Room, "Modulo3");
+    Room *quintal = NEW(Room, "Quintal");
+
+    //instancia o player
+
+    Player *player = NEW(Player,"vicks, the Hit-Maker");
+    map->player = player;
+    map->player->itens = (Item*)malloc(5*sizeof(Item));
+
+    //instancia os itens
+    Item *dixava = NEW(Item,"dixavador");
+    modulo1->item = dixava;
+
+    Item *seda = NEW(Item,"seda");
+    modulo2->item = seda;
+
+    Item *maconha = NEW(Item,"maconha");
+    modulo3->item = maconha;
+
+    // coloca nosso grande amigo cachaça na cena!
+
+    arena->bad_guy = NEW(Monster, "O insaciavel kchaça");
+    Item *chave = NEW(Item,"chave");
+    arena->bad_guy->item = chave;
+
+    // aponta os endereços das salas
+    hall->west = modulo3;
+    hall->north = modulo1;
+    
+    modulo3->east = hall;
+
+    modulo1->west = arena;
+    modulo1->east = modulo2;
+    modulo1->south = hall;
+
+
+    arena->east = modulo1;
+    arena->south = quintal;
+
+    modulo2->west = modulo1;
+
+    quintal->north = arena;
+    quintal->lock = 1;
+
+    // começa o jogo colocando o personagem no hall
+    map->start = hall;
+    map->location = hall;
+
+    //Testes
+    //Item *banza = NEW(Item,"baseado");
+ 
+    Player_add_item(map->player,chave);
+    //Player_add_item(map->player,banza );
+
+    return 1;
+}
+
+void Map_graph(void *self) { 
+
+    Map *map = self;	
+    printf("\n");
+    printf("\n");    
+    printf("\n");    
+    if (map->location->north) {
+	printf("               ");
+	printf("%s\n",map->location->north->proto.description);
+	printf("                  |                  \n");
+	printf("                  |                  \n");
+	printf("                  |                  \n");
+    }
+    else {    	
+	printf("               não tem\n");
+	printf("                  |                  \n");
+	printf("                  |                  \n");
+	printf("                  |                  \n");
+    }
+    if (map->location->west) {	     
+	 printf("%s",map->location->west->proto.description);    	 
+	 printf("———————");     
+    }
+    else {
+	printf("não tem——————");
+    }
+    printf("%s",map->location->proto.description);            
+    
+    if (map->location->east) { 	
+	printf("——————");
+	map->location->_(describe)(map->location->east);    
+    }
+    else {
+	printf("——————não tem\n");
+    }
+    printf("                  |                  \n");
+    printf("                  |                  \n");
+    printf("                  |                  \n");    
+    if (map->location->south) {	    
+	    printf("               ");
+	    printf("%s",map->location->south->proto.description);    	 
+	printf("               \n");
+    }
+    else {
+	printf("               não tem               \n");
+    }
+}
 
 
 
 Object MapProto = {
-    .init = Map_init,
-    .move = Map_move,
-    .attack = Map_attack
+.init = Map_init,
+.move = Map_move,
+.attack = Map_attack
 };
+
+
+
+
+int isGameOver(void *self) { 
+
+	Map *game = self;
+	Player *player = game->player;
+
+	if (Player_itemValidate(player,"chave"))
+		return 1;
+	else
+		return 0;
+}
 
 
 void help () {
 
-    printf("\n");
-    printf("\t Comandos:\n");
-    printf("\t\t l: lista os vizinhos\n");
-    printf("\t\t m: mapa dos vizinhos\n");
-    printf("\t\t i: informações do player\n");
-    printf("\t\t k: inventario\n");
-    printf("\t\t g: pega um item da sala\n");
-    printf("\t\t a: ataca!\n");
-    printf("\t\t j: enrola um baseado\n");
+printf("\n");
+printf("\t Comandos:\n");
+printf("\t\t l: lista os vizinhos\n");
+printf("\t\t m: mapa dos vizinhos\n");
+printf("\t\t i: informações do player\n");
+printf("\t\t k: inventario\n");
+printf("\t\t g: pega um item da sala\n");
+printf("\t\t a: ataca!\n");
+printf("\t\t j: enrola um baseado\n");
 }
 
 
@@ -523,7 +542,12 @@ void Arte_ascii() {
 int process_input(Map *game)
 {
 
-    printf("\n> ");
+   	if (isGameOver(game)) {
+   		printf("Parabéns! GG WP\n");
+   		return 0;
+   	}
+   	
+   	printf("\n> ");
 
     char ch = getchar();
     if (ch == '\n')
